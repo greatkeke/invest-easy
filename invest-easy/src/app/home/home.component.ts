b218234974491a4ee3138c7d26c8a04b82f3524f
greatkeke <fengxxxke@gmail.com>
2025-05-05 07:32:28 +0800

    Add cards in home

--- invest-easy/src/app/home/home.component.ts
+++ invest-easy/src/app/home/home.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  template: `<h1>首页</h1>`
+  imports: [MatButtonModule, MatCardModule, MatIconModule, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  showPromotions = true;
+  promotions = [
+    {
+      title: '新用户优惠',
+      description: '注册即送100元投资券',
+      visible: true
+    },
+    {
+      title: '限时特惠',
+      description: '部分基金手续费5折',
+      visible: true
+    },
+    {
+      title: '推荐有礼',
+      description: '邀请好友各得50元',
+      visible: true
+    }
+  ];
+
+  closePromotion(index: number) {
+    this.promotions[index].visible = false;
+  }
+}
