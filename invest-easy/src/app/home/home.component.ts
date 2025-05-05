e1c16e52a9847bb259f0316fa43a67831a1a914b
greatkeke <fengxxxke@gmail.com>
2025-05-05 15:34:49 +0800

    List recent market news in home

--- invest-easy/src/app/home/home.component.ts
+++ invest-easy/src/app/home/home.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsService } from '../news/news.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatIconModule, CommonModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, CommonModule, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   showPromotions = true;
   promotions = [
     {
@@ -30,6 +32,17 @@ export class HomeComponent {
       visible: true
     }
   ];
+
+  marketNews: any[] = [];
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.newsService.getMarketNews().subscribe({
+      next: (data) => this.marketNews = data.articles || [],
+      error: (err) => console.error('Failed to load news:', err)
+    });
+  }
 
   closePromotion(index: number) {
     this.promotions[index].visible = false;
