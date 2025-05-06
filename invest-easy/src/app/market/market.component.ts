99679d8ff6ff0e018f11cd72a4b9e203f33597e3
greatkeke <fengxxxke@gmail.com>
2025-05-06 14:42:36 +0800

    List main markets index

--- invest-easy/src/app/market/market.component.ts
+++ invest-easy/src/app/market/market.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarketService } from './market.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-market',
   standalone: true,
-  templateUrl: './market.component.html'
+  templateUrl: './market.component.html',
+  styleUrls: ['./market.component.scss'],
+  imports: [CommonModule, MatCardModule]
 })
-export class MarketComponent {}
+export class MarketComponent implements OnInit {
+  indices: any[] = [];
+  loading = true;
+
+  constructor(private marketService: MarketService) {}
+
+  ngOnInit(): void {
+    this.marketService.getMarketIndices().subscribe({
+      next: (response) => {
+        if (response.data && response.data.diff) {
+          this.indices = response.data.diff.map((item: any) => ({
+            name: this.getMarketName(item.f12),
+            symbol: item.f12,
+            price: item.f2,
+            change: item.f4,
+            percent: item.f3
+          }));
+        }
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  private getMarketName(symbol: string): string {
+    const names: Record<string, string> = {
+      '000001': '上证指数',
+      '399001': '深证成指',
+      'HSI': '恒生指数',
+      'DJIA': '道琼斯',
+      'NDX': '纳斯达克',
+      'SPX': '标普500'
+    };
+    return names[symbol] || symbol;
+  }
+}
---
+++ invest-easy/src/app/market/market.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface MarketIndex {
+  name: string;
+  symbol: string;
+  price: number;
+  change: number;
+  percent: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarketService {
+  private apiUrl = 'https://push2.eastmoney.com/api/qt/ulist.np/get';
+
+  constructor(private http: HttpClient) { }
+
+  getMarketIndices(): Observable<any> {
+    const params = {
+      fltt: '2',
+      secids: '1.000001,0.399001,100.HSI,100.DJIA,100.NDX,100.SPX',
+      fields: 'f2,f3,f4,f12,f14',
+      _: Date.now()
+    };
+    return this.http.get(this.apiUrl, { params });
+  }
+}
