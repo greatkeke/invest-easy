4581a1cfb2ae8999f6f1f10e01bff45b8097f1cb
greatkeke <fengxxxke@gmail.com>
2025-05-08 13:01:17 +0800

    Refactor user-panel for dialog

--- invest-easy/src/app/user-panel/user-panel.component.ts
+++ invest-easy/src/app/user-panel/user-panel.component.ts
@@ -1,13 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
+import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-panel',
@@ -16,18 +11,8 @@ import { map, shareReplay } from 'rxjs/operators';
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    MatSidenavModule,
     MatListModule,
     MatIconModule,
-    AsyncPipe,
   ]
 })
-export class UserPanelComponent {
-  private breakpointObserver = inject(BreakpointObserver);
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-}
+export class UserPanelComponent {}
