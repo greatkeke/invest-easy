abcab3ac99143bb91cb19e3d00e27be745ca561e
greatkeke <fengxxxke@gmail.com>
2025-05-10 10:13:29 +0800

    Add header in news

--- invest-easy/src/app/news/news.component.ts
+++ invest-easy/src/app/news/news.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsService } from './news.service';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
   marketNews: any[] = [];
 
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
     this.newsService.getMarketNews().subscribe({
