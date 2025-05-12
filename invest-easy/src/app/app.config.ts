ea767e681cdc0f60226842e3430add41435e5738
greatkeke <fengxxxke@gmail.com>
2025-05-12 15:13:53 +0800

    Use Noir as preset

--- invest-easy/src/app/app.config.ts
+++ invest-easy/src/app/app.config.ts
@@ -8,7 +8,56 @@ import { provideServiceWorker } from '@angular/service-worker';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import Material  from '@primeng/themes/material';
+import { definePreset } from '@primeng/themes';
+
+const appPreset = definePreset(Material, {
+  semantic: {
+      primary: {
+          50: '{zinc.50}',
+          100: '{zinc.100}',
+          200: '{zinc.200}',
+          300: '{zinc.300}',
+          400: '{zinc.400}',
+          500: '{zinc.500}',
+          600: '{zinc.600}',
+          700: '{zinc.700}',
+          800: '{zinc.800}',
+          900: '{zinc.900}',
+          950: '{zinc.950}'
+      },
+      colorScheme: {
+          light: {
+              primary: {
+                  color: '{zinc.950}',
+                  inverseColor: '#ffffff',
+                  hoverColor: '{zinc.900}',
+                  activeColor: '{zinc.800}'
+              },
+              highlight: {
+                  background: '{zinc.950}',
+                  focusBackground: '{zinc.700}',
+                  color: '#ffffff',
+                  focusColor: '#ffffff'
+              }
+          },
+          dark: {
+              primary: {
+                  color: '{zinc.50}',
+                  inverseColor: '{zinc.950}',
+                  hoverColor: '{zinc.100}',
+                  activeColor: '{zinc.200}'
+              },
+              highlight: {
+                  background: 'rgba(250, 250, 250, .16)',
+                  focusBackground: 'rgba(250, 250, 250, .24)',
+                  color: 'rgba(255,255,255,.87)',
+                  focusColor: 'rgba(255,255,255,.87)'
+              }
+          }
+      }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient(), provideServiceWorker('ngsw-worker.js', {
@@ -18,7 +67,7 @@ export const appConfig: ApplicationConfig = {
   provideAnimationsAsync(),
   providePrimeNG({
     theme: {
-      preset: Aura
+      preset: appPreset 
     },
     ripple: true,
   })
