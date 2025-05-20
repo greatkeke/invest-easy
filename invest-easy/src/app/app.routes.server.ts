3eee4e2827a9d5e0a740834ba9de4bc5873abc50
greatkeke <fengxxxke@gmail.com>
2025-05-20 10:50:50 +0800

    Specify to client rendermode

--- invest-easy/src/app/app.routes.server.ts
+++ invest-easy/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'transfer/:tab',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'instrument/:symbol',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
