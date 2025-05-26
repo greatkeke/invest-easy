5b6ebfdb178ea54ac68ebb6a80c27ff9ff910912
greatkeke <fengxxxke@gmail.com>
2025-05-26 14:22:34 +0800

    Add loading animation

--- invest-easy/src/app/asset-detail/asset-detail.component.ts
+++ invest-easy/src/app/asset-detail/asset-detail.component.ts
@@ -3,23 +3,32 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { TopNavigationComponent } from '../shared/top-navigation/top-navigation.component';
 import { HistoryComponent } from '../shared/history/history.component';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-asset-detail',
   standalone: true,
-  imports: [CommonModule, ButtonModule, TopNavigationComponent, HistoryComponent],
+  imports: [CommonModule, ButtonModule, TopNavigationComponent, HistoryComponent, ProgressSpinnerModule],
   templateUrl: './asset-detail.component.html',
   styleUrl: './asset-detail.component.scss'
 })
 export class AssetDetailComponent {
+  isLoading = false;
 
   goBack() {
 
   }
 
-  refreshData() {
-    console.log('refresh data....')
+  ngOnInit(): void {
+    this.isLoading = true;
+    // Simulate data loading
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1500);
+
   }
 
-
+  refreshData() {
+    console.log('refresh data....');
+  }
 }
