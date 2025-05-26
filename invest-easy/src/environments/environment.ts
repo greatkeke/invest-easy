173abf0c9676d77a6fa192d234fea81fad92357d
greatkeke <fengxxxke@gmail.com>
2025-05-26 14:47:50 +0800

    Add development environment

---
+++ invest-easy/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  newsApiKey: '********************', // Replace with actual key
+  apiBaseUrl: '********************' // Development API base URL
+};
