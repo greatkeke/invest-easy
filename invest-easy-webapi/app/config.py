25534c9e0975b77a31f44f11e8edee0262de7d27
greatkeke <fengxxxke@gmail.com>
2025-05-29 22:37:36 +0800

    Support latet label

--- invest-easy-webapi/app/config.py
+++ invest-easy-webapi/app/config.py
@@ -1,18 +1,18 @@
 from functools import lru_cache
+import os
 from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import logging
 
-
 class Settings(BaseSettings):
+    allow_origins: str = os.getenv("allow_origins", "*")
     model_config = SettingsConfigDict(env_file=".env")
-    allow_origins: str = "*"
 
     @property
     def allow_origins_list(self) -> List[str]:
         list = [origin.strip() for origin in self.allow_origins.split(",")]
         for item in list:
-            logging.info(f"Allow origin: {item}")
+            logging.warning(f"Allow origin: {item}")
         return list
 
 
