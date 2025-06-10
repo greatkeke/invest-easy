8adf8f620dd469af2e84f4e1f03b871edadaeb76
greatkeke <fengxxxke@gmail.com>
2025-06-10 13:08:02 +0800

    Refactor asset-detail

--- invest-easy/src/app/asset-detail/asset-detail.component.ts
+++ invest-easy/src/app/asset-detail/asset-detail.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { AccordionModule } from 'primeng/accordion';
 import { TopNavigationComponent } from '../shared/top-navigation/top-navigation.component';
 import { HistoryComponent } from '../history/history.component';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { BalanceService } from '../shared/api-services/balance.service';
 import { AccountBalance, AccountsService } from '../shared/api-services/accounts.service';
 
 @Component({
   selector: 'app-asset-detail',
   standalone: true,
-  imports: [CommonModule, ButtonModule, TopNavigationComponent, HistoryComponent, ProgressSpinnerModule],
+  imports: [CommonModule, ButtonModule, TableModule, TopNavigationComponent, HistoryComponent, ProgressSpinnerModule, AccordionModule],
   templateUrl: './asset-detail.component.html',
   styleUrl: './asset-detail.component.scss'
 })
 export class AssetDetailComponent {
   isLoading = false;
   accountBalances: AccountBalance[] = [];
+  expand_id: string = "";
 
   constructor(
     private accountsService: AccountsService
@@ -29,6 +31,7 @@ export class AssetDetailComponent {
 
   async ngOnInit(): Promise<void> {
     await this.loadAccountBalances();
+    this.expand_id = this.accountBalances[0].id;
   }
 
   async loadAccountBalances(): Promise<void> {
