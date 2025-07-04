6dd71eb77e7b221df9919d01dcac4618812b3ef6
greatkeke <fengxxxke@gmail.com>
2025-07-04 13:59:24 +0800

    Tweak theming

--- invest-easy/src/app/app.config.ts
+++ invest-easy/src/app/app.config.ts
@@ -17,39 +17,39 @@ import { catchError, firstValueFrom, tap } from 'rxjs';
 const appPreset = definePreset(Material, {
   semantic: {
     primary: {
-      50: '{zinc.50}',
-      100: '{zinc.100}',
-      200: '{zinc.200}',
-      300: '{zinc.300}',
-      400: '{zinc.400}',
-      500: '{zinc.500}',
-      600: '{zinc.600}',
-      700: '{zinc.700}',
-      800: '{zinc.800}',
-      900: '{zinc.900}',
-      950: '{zinc.950}'
+      50: '{red.50}',
+      100: '{red.100}',
+      200: '{red.200}',
+      300: '{red.300}',
+      400: '{red.400}',
+      500: '{red.500}',
+      600: '{red.600}',
+      700: '{red.700}',
+      800: '{red.800}',
+      900: '{red.900}',
+      950: '{red.950}'
     },
     colorScheme: {
       light: {
         primary: {
-          color: '{zinc.950}',
+          color: '{red.950}',
           inverseColor: '#ffffff',
-          hoverColor: '{zinc.900}',
-          activeColor: '{zinc.800}'
+          hoverColor: '{red.900}',
+          activeColor: '{red.800}'
         },
         highlight: {
-          background: '{zinc.950}',
-          focusBackground: '{zinc.700}',
+          background: '{red.950}',
+          focusBackground: '{red.700}',
           color: '#ffffff',
           focusColor: '#ffffff'
         }
       },
       dark: {
         primary: {
-          color: '{zinc.50}',
-          inverseColor: '{zinc.950}',
-          hoverColor: '{zinc.100}',
-          activeColor: '{zinc.200}'
+          color: '{red.50}',
+          inverseColor: '{red.950}',
+          hoverColor: '{red.100}',
+          activeColor: '{red.200}'
         },
         highlight: {
           background: 'rgba(250, 250, 250, .16)',
