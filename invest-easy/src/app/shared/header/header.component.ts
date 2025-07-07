c175c6a70fe642b152d56d89ef76fdf05a0f122d
greatkeke <fengxxxke@gmail.com>
2025-07-07 09:04:37 +0800

    Adapter location change for notification

--- invest-easy/src/app/shared/header/header.component.ts
+++ invest-easy/src/app/shared/header/header.component.ts
@@ -1,30 +1,52 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { CommonModule, NgStyle } from '@angular/common';
-import { Router } from '@angular/router';
+import { CommonModule, Location, NgStyle } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DialogModule } from 'primeng/dialog';
 import { NotificationCenterComponent } from '../../notification-center/notification-center.component';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [ButtonModule, CommonModule, DialogModule, NotificationCenterComponent, NgStyle],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @Input() title = 'HSBC'; // Default value
+  @Input() title = 'Invest-Easy'; // Default value
   @Input() bg_img = 'financial-regulation-header.jpg';
   displayUserPanel = false;
   displayNotificationCenter = false;
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
   ) { }
 
+  ngOnInit() {
+    this.route.queryParamMap.subscribe(params => {
+      const ntf = params.get('notification');
+      if (!!ntf && ntf == "on") {
+        this.displayNotificationCenter = true;
+      }
+      else {
+        this.displayNotificationCenter = false;
+      }
+    })
+  }
+
   goToNotifications() {
-    // Show notification dialog
     this.displayNotificationCenter = true;
+
+    const params = new HttpParams().appendAll({ notification: "on" });
+    this.location.go(location.pathname, params.toString());
+  }
+
+  onClose() {
+    const params = new HttpParams();
+    this.location.replaceState(location.pathname, params.toString());
   }
 
   toggleUserPanel() {
